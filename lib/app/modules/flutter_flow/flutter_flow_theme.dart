34be800f2f516a3a81338292104b5bddd50236a9
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2021-07-03 16:26:47 -0300

    Home

---
+++ lib/app/modules/flutter_flow/flutter_flow_theme.dart
@@ -0,0 +1,35 @@
+// TODO Implement this library.
+
+import 'package:flutter/material.dart';
+
+class FlutterFlowTheme {
+  static Color primaryColor = Color(0xffc62828);
+
+  static TextStyle subtitle1 = TextStyle(
+    color: Color(0xFF3f3f3f),
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+    fontFamily: 'Poppins',
+  );
+
+  static TextStyle subtitle2 = TextStyle(
+    color: Color(0xFFa9ada4),
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle title1 = TextStyle(
+    color: Color(0xFF3f3f3f),
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle bodyText1 = TextStyle(
+    color: Color(0xFF3f3f3f),
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    fontFamily: 'Roboto',
+  );
+}
