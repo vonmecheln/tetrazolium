d4db33b80ad314079cf65da6309862b3ecfbd43d
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2021-07-03 16:47:35 -0300

    novo

--- lib/app/modules/novo/novo_page.dart
+++ lib/app/modules/novo/novo_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:tetrazolium/app/constants.dart';
 
 class NovoPage extends StatefulWidget {
@@ -14,6 +15,32 @@ class _NovoPageState extends State<NovoPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("ANÁLISES"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(),
+            TextField(),
+            TextField(),
+            TextField(),
+            TextField(),
+            Expanded(
+                child: Container(
+              color: Colors.red,
+            )),
+            Container(
+              height: 100,
+              // color: Colors.blue,
+              child: ElevatedButton(
+                child: Text('Salvar'),
+                onPressed: () {
+                  Modular.to.pushNamed('analise');
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
