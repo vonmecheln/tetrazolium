12818348e65ec17bf69bad2357d32402c2338b08
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2021-07-04 19:51:20 -0300

    Analise class

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -3,12 +3,53 @@
 // import 'package:flutter_triple/flutter_triple.dart';
 // import 'home_store.dart';
 
+import 'dart:math';
+
 import 'package:flutter_modular/flutter_modular.dart';
 
 import '../flutter_flow/flutter_flow_theme.dart';
 // import '../flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+int _intInRange(Random source, int min, int max) =>
+    source.nextInt(max - min) + min;
+final random = new Random();
+final analises = List.generate(
+  10,
+  (index) => Analise(
+    lote: '',
+    data: DateTime(2021),
+    vigor: _intInRange(random, 60, 99),
+    viabilidade: _intInRange(random, 60, 99),
+    mecanico: _intInRange(random, 0, 20),
+    umidade: _intInRange(random, 0, 20),
+    bug: _intInRange(random, 0, 20),
+    dura: _intInRange(random, 0, 20),
+  ),
+);
+
+class Analise {
+  final String lote;
+  final DateTime data;
+  final int vigor;
+  final int viabilidade;
+  final int mecanico;
+  final int umidade;
+  final int bug;
+  final int dura;
+
+  Analise({
+    required this.lote,
+    required this.data,
+    required this.vigor,
+    required this.viabilidade,
+    required this.mecanico,
+    required this.umidade,
+    required this.bug,
+    required this.dura,
+  });
+}
 
 class HomePageWidget extends StatefulWidget {
   HomePageWidget({Key? key}) : super(key: key);
@@ -48,7 +89,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
             // shrinkWrap: true,
             // physics: BouncingScrollPhysics(),
 
-            itemCount: 2,
+            itemCount: analises.length,
             itemBuilder: (_, __) {
               return TetraCard();
             }),
@@ -85,12 +126,7 @@ class CardC extends StatelessWidget {
     return ExpansionTile(
       title: PainelAnalise(),
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            PainelDanos(),
-          ],
-        ),
+        PainelDanos(),
       ],
     );
   }
@@ -134,19 +170,17 @@ class PainelDanos extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: Expanded(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              DisplayDano(),
-              DisplayDano(),
-              DisplayDano(),
-              DisplayDano(),
-            ],
-          ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            DisplayDano(),
+            DisplayDano(),
+            DisplayDano(),
+            DisplayDano(),
+          ],
         ),
       ),
     );
