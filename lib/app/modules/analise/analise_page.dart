f278730d7ac90cc34afe4925552daca2f12a7ca4
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2021-07-04 19:51:42 -0300

    Coleta Class

--- lib/app/modules/analise/analise_page.dart
+++ lib/app/modules/analise/analise_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:tetrazolium/app/modules/analise/analise_store.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +10,7 @@ class AnalisePage extends StatefulWidget {
   @override
   AnalisePageState createState() => AnalisePageState();
 }
+
 class AnalisePageState extends State<AnalisePage> {
   final AnaliseStore store = Modular.get();
 
@@ -22,4 +25,42 @@ class AnalisePageState extends State<AnalisePage> {
       ),
     );
   }
+}
+
+int _intInRange(Random source, int min, int max) =>
+    source.nextInt(max - min) + min;
+final random = new Random();
+final analises = List.generate(
+  10,
+  (index) => Coleta(
+    imageE: '',
+    imageI: '',
+    mecanico: _intInRange(random, 0, 20),
+    umidade: _intInRange(random, 0, 20),
+    bug: _intInRange(random, 0, 20),
+    dura: _intInRange(random, 0, 20),
+    classificacao: _intInRange(random, 1, 8),
+  ),
+);
+
+class Coleta {
+  final String imageE;
+  final String imageI;
+  final int mecanico;
+  final int umidade;
+  final int bug;
+  final int dura;
+  final int classificacao;
+
+  Coleta({
+    required this.imageE,
+    required this.imageI,
+    required this.mecanico,
+    required this.umidade,
+    required this.bug,
+    required this.dura,
+    required this.classificacao,
+  });
+
+  //String get dataf => DateFormat('dd/MM/yyyy').format(data);
 }
