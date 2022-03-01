eb8d4ab132c3da11bdc1826944acc071da7bf34e
Luis Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2022-03-01 20:17:25 -0300

    Move external to shared

--- lib/app/modules/analysis/analysis_module.dart
+++ lib/app/modules/analysis/analysis_module.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:tetrazolium/app/modules/analysis/domain/usecases/get_analysis_realtime.dart';
-import 'package:tetrazolium/app/modules/analysis/external/datasource/analysis_datasource_impl.dart';
 import 'package:tetrazolium/app/modules/analysis/infra/repositories/analise_repository_impl.dart';
 import 'package:tetrazolium/app/modules/analysis/presenter/pages/details_page.dart';
 import 'package:tetrazolium/app/modules/analysis/presenter/stores/list_store.dart';
+import 'package:tetrazolium/app/shared/external/datasource/analysis_datasource_impl.dart';
 import 'presenter/pages/list_page.dart';
 
 class AnalysisModule extends Module {
